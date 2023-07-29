38894fa5006a04c9c0b10b2d0e87c79a85623c3a
Mlungisi Jali <msincuba@gmail.com.com>
2023-07-29 11:26:04 +0200

    Angular multiselect clone

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular2-multiselect-dropdown'),
+      dir: require('path').join(__dirname, './coverage/angular-multiselect-dropdown'),
       subdir: '.',
       reporters: [
         { type: 'html' },
